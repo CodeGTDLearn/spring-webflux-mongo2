053f9a750bf9535e120e12dcacbe2da0ac21cca8
PauloPortofolio <pvba02@gmail.com>
2021-11-23 15:30:10 -0500

    1) Starting the project
     - Added README.md
     - Pom dependencies

---
+++ src/test/java/com/webflux/mongo2/ApiDriverTests.java
@@ -0,0 +1,13 @@
+package com.webflux.mongo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDriverTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
