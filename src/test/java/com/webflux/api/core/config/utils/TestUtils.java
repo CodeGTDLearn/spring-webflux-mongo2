74bdec97e5b6b0630a9c923bbc3285a824baf134
PauloPortofolio <pvba02@gmail.com>
2023-03-09 19:38:41 -0500

    6.1) Refactoring
    -> CONCLUDED:
    Test/application.yml comments
    TransactionsClassicExcProjectEmpty
    Move global exceptions to CORE folder
    Change from Java 11 to JAva 17
    
    -> WORKING:
    Implant Class PanelClass in TestUtils
    
    -> NEXT STEPS:
    Cacheable
    Modular Monolith
    Jenkins Pipeline + Reverse Proxy
    
    -> FUTURE IMPLEMENTATIONS:
    Internationalization
    Exceptions - identify (Http 500, 400, 200)
    Upload: Pictures + Files
    GPS
    Push notifications
    OAuth2/JWT authentication
    Multiples DB's
    Multi-Tenancy

--- src/test/java/com/webflux/api/core/config/utils/TestUtils.java
+++ src/test/java/com/webflux/api/core/config/utils/TestUtils.java
@@ -7,9 +7,14 @@ import org.junit.jupiter.api.BeforeAll;
 import org.testcontainers.containers.DockerComposeContainer;
 import org.testcontainers.containers.MongoDBContainer;
 
+import java.util.ArrayList;
+
 import static com.webflux.api.core.config.utils.BlockhoundUtils.blockhoundInstallWithSpecificAllowedCalls;
 import static com.webflux.api.core.config.utils.RestAssureSpecs.requestSpecs;
 import static com.webflux.api.core.config.utils.RestAssureSpecs.responseSpecs;
+import static java.lang.Math.min;
+import static java.lang.System.out;
+import static java.util.stream.Stream.of;
 
 @Slf4j
 public class TestUtils {
@@ -64,13 +69,10 @@ public class TestUtils {
     }
 
 
-    System.out.printf(
+    out.printf(
 
-         "╔════════════════════════════════════════════════════════════════════╗\n" +
-         "║                       %s                                           ║\n" +
-         "║ --> Name: %s %38s%n" +
-         "╚════════════════════════════════════════════════════════════════════╝\n"
-         ,
+         "╔════════════════════════════════════════════════════════════════════╗\n" + "║         "
+         + "              %s                                           ║\n" + "║ --> Name: %s " + "%38s%n" + "╚════════════════════════════════════════════════════════════════════╝\n",
          title, subTitle, "║"
     );
   }
@@ -95,46 +97,26 @@ public class TestUtils {
       }
 
 
-      System.out.printf(
-           "╔═══════════════════════════════════════════════════════════════════════╗\n" +
-           "║ --> Name: %s\n" +
-           "║ --> Url: %s\n" +
-           "║ --> Running: %s\n" +
-           "╚═══════════════════════════════════════════════════════════════════════╝\n\n"
-           ,
-           title,
-           container.getContainerName(),
-           container.getReplicaSetUrl(),
-           container.isRunning()
+      out.printf(
+           "╔═══════════════════════════════════════════════════════════════════════╗\n" + "║ " + "-->" + " Name: %s\n" + "║ --> Url: %s\n" + "║ --> Running: %s\n" + "╚═══════════════════════════════════════════════════════════════════════╝\n\n",
+           title, container.getContainerName(), container.getReplicaSetUrl(), container.isRunning()
       );
     }
   }
 
 
-  public static void globalComposeServiceContainerMessage(
-       DockerComposeContainer<?> compose,
-       String service,
-       Integer port) {
+  public static void globalComposeServiceContainerMessage(DockerComposeContainer<?> compose,
+                                                          String service, Integer port) {
 
     if (compose != null) {
-      System.out.printf(
+      out.printf(
 
-           "╔═══════════════════════════════════════════════════════════════════════\n" +
-           "║                           %s                        ║\n" +
-           "║ --> Service: %s\n" +
-           "║ --> Host: %s\n" +
-           "║ --> Port: %s\n" +
-           "║ --> Created: %s\n" +
-           "║ --> Running: %s\n" +
-           "╚═══════════════════════════════════════════════════════════════════════\n\n"
-           ,
-           "TC-CONTAINER-COMPOSE",
-           service,
-           compose.getServiceHost(service, port),
-           compose.getServicePort(service, port),
-           compose.getContainerByServiceName(service + "_1")
-                  .get()
-                  .isCreated(),
+           "╔═══════════════════════════════════════════════════════════════════════\n" + "║     "
+           + "                      %s                        ║\n" + "║ --> Service: %s\n" + "║ " + "-->" + " Host: %s\n" + "║ --> Port: %s\n" + "║ --> Created: %s\n" + "║ --> Running: " + "%s\n" + "╚═══════════════════════════════════════════════════════════════════════\n\n",
+           "TC-CONTAINER-COMPOSE", service, compose.getServiceHost(service, port),
+           compose.getServicePort(service, port), compose.getContainerByServiceName(service + "_1")
+                                                         .get()
+                                                         .isCreated(),
            compose.getContainerByServiceName(service + "_1")
                   .get()
                   .isRunning()
@@ -142,5 +124,283 @@ public class TestUtils {
     }
   }
 
+  private static class ConsolePanel {
+
+    public static void simplePanel(String... texts) {
+      // @formatter:off
+      panel(45,
+            5,
+            1,
+            1,
+            Border.DOUBLE,
+            Border.DOUBLE,
+            Border.THIN,
+            Border.THIN,
+            true,
+            true, texts);
+      // @formatter:on
+    }
+
+    public static void simplePanelScalable(int scale, String... texts) {
+      // @formatter:off
+      panel(scale,
+            5,
+            1,
+            1,
+            Border.DOUBLE,
+            Border.DOUBLE,
+            Border.THIN,
+            Border.THIN,
+            true,
+            true,
+            texts
+      );
+      // @formatter:on
+    }
+
+    // @formatter:off
+    public static void panel(
+         int scale,
+         int margin,
+         int upSpace,
+         int downSpace,
+         Border cornersFormat, Border centerMarksFormat,
+         Border horizontalLinesFormat,
+         Border verticalLinesFormat,
+         boolean uppercaseTitle,
+         boolean centralizeTitle,
+         String... titleAndOthers) {
+      // @formatter:on
+
+      var estimatedAdjustmentFactor = 3;
+      var title = titleAndOthers[0];
+      var marginTitle = scale - (title.length() / 2) - estimatedAdjustmentFactor;
+      var formattedTexts = of(titleAndOthers).map(
+                                                  item -> item.equals(title) && centralizeTitle ?
+                                                       " ".repeat(marginTitle) + title : item)
+                                             .map(item -> item.equals(
+                                                  title) && uppercaseTitle ? item.toUpperCase() :
+                                                  item)
+                                             .toArray();
+
+      var marginLimitedBySize = min(margin, scale);
+
+      // scale + margin discrepacies eliminated
+      if (marginLimitedBySize % 2 != 0) -- marginLimitedBySize;
+      if (scale % 2 != 0) ++ scale;
+
+      int fullSize = (scale * 2) - marginLimitedBySize;
+      if (fullSize % 2 == 0) ++ fullSize;
+      else -- fullSize;
+
+      var whitespaceMargin = " ".repeat(marginLimitedBySize);
+      var externalUpSpaces = "\n".repeat(upSpace);
+      var externalBottomSpaces = "\n".repeat(downSpace);
+
+      var upperFace = upperLine(scale, cornersFormat, centerMarksFormat, horizontalLinesFormat);
+      var divider = middleLine(scale, cornersFormat, centerMarksFormat, horizontalLinesFormat);
+      var bottomFace = bottomLine(scale, cornersFormat, centerMarksFormat, horizontalLinesFormat);
+      var faceLine = faceLine(verticalLinesFormat);
+
+      var titleTextArea = String.valueOf(fullSize);
+      var textPreparation = new StringBuilder();
+      textPreparation.append(externalUpSpaces)
+                     .append(upperFace)
+                     .append(faceLine)
+                     .append("%s%%-%ss".formatted(whitespaceMargin, titleTextArea))
+                     .append(faceLine)
+                     .append("\n")
+                     .append(divider);
+
+      // "-1" Because the first element in the Array was used as title
+      // The discont-number in bodyTextArea/fullsize, subtract the size of "ordinal-ASC" and ") "
+      var bodyTextArea = String.valueOf(fullSize - 4);
+      var topicEnumeration = 0;
+      var ordinalSymbolEnumerator = '\u2070';
+      for (int i = formattedTexts.length - 1; i > 0; i--) {
+        ++ topicEnumeration;
+        textPreparation.append(faceLine)
+                       .append("%s%s%s) %%-%ss".formatted(whitespaceMargin, topicEnumeration,
+                                                          ordinalSymbolEnumerator, bodyTextArea
+                       ))
+                       .append(faceLine)
+                       .append("\n");
+      }
+      textPreparation.append(bottomFace)
+                     .append(externalBottomSpaces);
+      out.printf(textPreparation.toString(), formattedTexts);
+    }
+
+    private static String generateLine(char baseChar, int scale, char BASE_LINE) {
+
+      return String.valueOf(baseChar)
+                   .repeat(scale)
+                   .replace(baseChar, BASE_LINE);
+    }
+
+    private static String upperLine(int scale, Border corner, Border centerMark, Border line) {
+
+      ArrayList<Character> borderStylingItems = new ArrayList<>();
+      switch (corner) {
+        case BOLD -> {
+          borderStylingItems.add(BoldFont.UPPER_LEFT_CORNER.code);
+          borderStylingItems.add(BoldFont.UPPER_RIGHT_CORNER.code);
+        }
+        case THIN -> {
+          borderStylingItems.add(ThinFont.UPPER_LEFT_CORNER.code);
+          borderStylingItems.add(ThinFont.UPPER_RIGHT_CORNER.code);
+        }
+        case DOUBLE -> {
+          borderStylingItems.add(DoubleFont.UPPER_LEFT_CORNER.code);
+          borderStylingItems.add(DoubleFont.UPPER_RIGHT_CORNER.code);
+        }
+      }
+
+      switch (centerMark) {
+        case BOLD -> borderStylingItems.add(BoldFont.BASE_LINE.code);
+        case THIN -> borderStylingItems.add(ThinFont.BASE_LINE.code);
+        case DOUBLE -> borderStylingItems.add(DoubleFont.BASE_LINE.code);
+      }
+
+      switch (line) {
+        case BOLD -> borderStylingItems.add(BoldFont.BASE_LINE.code);
+        case THIN -> borderStylingItems.add(ThinFont.BASE_LINE.code);
+        case DOUBLE -> borderStylingItems.add(DoubleFont.BASE_LINE.code);
+      }
+
+      var baseline = generateLine('_', scale, borderStylingItems.get(3));
+
+      return borderStylingItems.get(0) + baseline + borderStylingItems.get(
+           2) + baseline + borderStylingItems.get(1) + "\n";
+    }
+
+    private static String middleLine(int scale, Border corner, Border centerMark,
+                                     Border baseLine) {
+
+      ArrayList<Character> borderStylingItems = new ArrayList<>();
+      switch (corner) {
+        case BOLD -> {
+          borderStylingItems.add(BoldFont.MIDDLE_LEFT.code);
+          borderStylingItems.add(BoldFont.MIDDLE_RIGHT.code);
+        }
+        case THIN -> {
+          borderStylingItems.add(ThinFont.MIDDLE_LEFT.code);
+          borderStylingItems.add(ThinFont.MIDDLE_RIGHT.code);
+        }
+        case DOUBLE -> {
+          borderStylingItems.add(DoubleFont.MIDDLE_LEFT.code);
+          borderStylingItems.add(DoubleFont.MIDDLE_RIGHT.code);
+        }
+      }
+
+      switch (centerMark) {
+        case BOLD -> borderStylingItems.add(BoldFont.BASE_LINE.code);
+        case THIN -> borderStylingItems.add(ThinFont.BASE_LINE.code);
+        case DOUBLE -> borderStylingItems.add(DoubleFont.BASE_LINE.code);
+      }
+
+      switch (baseLine) {
+        case BOLD -> borderStylingItems.add(BoldFont.BASE_LINE.code);
+        case THIN -> borderStylingItems.add(ThinFont.BASE_LINE.code);
+        case DOUBLE -> borderStylingItems.add(DoubleFont.BASE_LINE.code);
+      }
+
+      var divider = generateLine('_', scale, borderStylingItems.get(3));
+
+      return borderStylingItems.get(0) + divider + borderStylingItems.get(
+           2) + divider + borderStylingItems.get(1) + "\n";
+    }
+
+    private static String bottomLine(int scale, Border corner, Border centerMark, Border baseLine) {
+
+      ArrayList<Character> borderStylingItems = new ArrayList<>();
+      switch (corner) {
+        case BOLD -> {
+          borderStylingItems.add(BoldFont.LOWER_LEFT_CORNER.code);
+          borderStylingItems.add(BoldFont.LOWER_RIGHT_CORNER.code);
+        }
+        case THIN -> {
+          borderStylingItems.add(ThinFont.LOWER_LEFT_CORNER.code);
+          borderStylingItems.add(ThinFont.LOWER_RIGHT_CORNER.code);
+        }
+        case DOUBLE -> {
+          borderStylingItems.add(DoubleFont.LOWER_LEFT_CORNER.code);
+          borderStylingItems.add(DoubleFont.LOWER_RIGHT_CORNER.code);
+        }
+      }
+
+      switch (centerMark) {
+        case BOLD -> borderStylingItems.add(BoldFont.BASE_LINE.code);
+        case THIN -> borderStylingItems.add(ThinFont.BASE_LINE.code);
+        case DOUBLE -> borderStylingItems.add(DoubleFont.BASE_LINE.code);
+      }
+
+      switch (baseLine) {
+        case BOLD -> borderStylingItems.add(BoldFont.BASE_LINE.code);
+        case THIN -> borderStylingItems.add(ThinFont.BASE_LINE.code);
+        case DOUBLE -> borderStylingItems.add(DoubleFont.BASE_LINE.code);
+      }
+
+      var baseline = generateLine('_', scale, borderStylingItems.get(3));
+
+      return borderStylingItems.get(0) + baseline + borderStylingItems.get(
+           2) + baseline + borderStylingItems.get(1) + "\n";
+    }
+
+    private static Character faceLine(Border corner) {
+
+      return switch (corner) {
+        case BOLD -> BoldFont.FACE_LINE.code;
+        case THIN -> ThinFont.FACE_LINE.code;
+        case DOUBLE -> DoubleFont.FACE_LINE.code;
+      };
+    }
+
+    private enum BoldFont {
+
+      FACE_LINE('\u2503'), BASE_LINE('\u2501'), UPPER_LEFT_CORNER('\u250F'), UPPER_RIGHT_CORNER(
+           '\u2513'), MIDDLE_LEFT('\u2523'), MIDDLE_RIGHT('\u252B'), LOWER_LEFT_CORNER(
+           '\u2517'), LOWER_RIGHT_CORNER('\u251B');
+
+      private final char code;
+
+      BoldFont(char code) {
+
+        this.code = code;
+      }
+    }
+
+    private enum ThinFont {
+
+      FACE_LINE('\u2502'), BASE_LINE('\u2500'), UPPER_LEFT_CORNER('\u250C'), UPPER_RIGHT_CORNER(
+           '\u2510'), MIDDLE_LEFT('\u252C'), MIDDLE_RIGHT('\u2524'), LOWER_LEFT_CORNER(
+           '\u2514'), LOWER_RIGHT_CORNER('\u2518');
+
+      private final char code;
+
+      ThinFont(char code) {
+
+        this.code = code;
+      }
+    }
+
+    private enum DoubleFont {
+
+      FACE_LINE('\u2551'), BASE_LINE('\u2550'), UPPER_LEFT_CORNER('\u2554'), UPPER_RIGHT_CORNER(
+           '\u2557'), MIDDLE_LEFT('\u2560'), MIDDLE_RIGHT('\u2563'), LOWER_LEFT_CORNER(
+           '\u255A'), LOWER_RIGHT_CORNER('\u255D');
+
+      private final char code;
+
+      DoubleFont(char code) {
+
+        this.code = code;
+      }
+    }
+
+    private enum Border {
+      BOLD, THIN, DOUBLE
+    }
+  }
 
 }
