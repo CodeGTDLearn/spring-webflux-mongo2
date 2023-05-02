0dcbc7b0f501047a07f9c2b43410272970ed9fd5
PauloPortofolio <pvba02@gmail.com>
2023-05-02 19:29:45 -0400

    6.6) Refactoring
    -> CONCLUDED:
    body("message", containsString("teste one message"))
    
    -> WORKING:
    Bean Validation Entity Messages - from yml (working on)
    
    -> NEXT STEPS:
    RestAssure check URLS
    Spliting exceptions:
    - Global
    - Bean Validation
    
    -> FUTURE IMPLEMENTATIONS:
    Cacheable
    Modular Monolith
    Jenkins Pipeline + Reverse Proxy
    Internationalization
    Exceptions - identify (Http 500, 400, 200)
    Upload: Pictures + Files
    GPS
    Push notifications
    OAuth2/JWT authentication
    Multiples DB's
    Multi-Tenancy

--- src/main/java/com/webflux/api/modules/project/entity/Project.java
+++ src/main/java/com/webflux/api/modules/project/entity/Project.java
@@ -1,9 +1,7 @@
 package com.webflux.api.modules.project.entity;
 
-import com.webflux.api.modules.project.core.exceptions.ProjectExceptionsMessages;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Version;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -21,15 +19,11 @@ public class Project implements Serializable {
 
   @Serial
   private static final long serialVersionUID = - 3086158644332115460L;
-  private final String NAME_EMPTY = messages.getProjectNameEmptyBeanValidationMessage();
-
-  @Autowired
-  private ProjectExceptionsMessages messages;
 
   @Id
   private String _id;
 
-  @NotEmpty(message = NAME_EMPTY)
+  @NotEmpty(message = "{modules.exception.project.nameEmptyMessage}" )
   private String name;
 
   private String code;
